1848ba2b439379d2e90469d8563efb634edc0e35
rupam <rexrupam70@gmail.com>
2025-05-18 11:59:53 +0530

    add endpoint to reply comment, delete comment

--- src/controller/youtube.controller.js
+++ src/controller/youtube.controller.js
@@ -35,14 +35,14 @@ export const callback = async (req, res) => {
       httpOnly: true,
       secure: true
     }
-
     return res
-      .cookie('accessToken', tokenResponse?.data?.access_token, options)
       .cookie('token', token, options)
       .status(200)
       .json({ message: "Successfully login with google" })
   } catch (error) {
-    return res.status(error?.tokenResponse?.data?.error?.code).json({ error: error?.tokenResponse?.data?.error?.message })
+    const code = error?.tokenResponse?.data?.error?.code;
+    const message = error?.tokenResponse?.data?.error?.message
+    return res.status(code).json({ message })
   }
 }
 
@@ -59,13 +59,13 @@ export const getVideos = async (req, res) => {
           Authorization: `Bearer ${token}`
         },
         params: {
-          part: 'snippet,contentDetails',
+          part: 'snippet',
           id: 'BuScJaRZxPg'  //BuScJaRZxPg
         }
       })
-    const videoTitle = response?.data?.items[0].snippet.title;
-    const videoDetails = response?.data?.items[0].contentDetails
-    return res.status(200).json({ videoTitle, videoDetails })
+    const title = response?.data?.items[0].snippet.title;
+    const description = response?.data?.items[0].snippet.description
+    return res.status(200).json({ title, description })
   } catch (error) {
     const status = error.response.data.error.code || 500
     const message = error.response.data.error.message || "Internal server error"
@@ -113,15 +113,13 @@ export const postComment = async (req, res) => {
 }
 
 export const changeTitle = async(req,res)=>{
-   //PUT https://www.googleapis.com/youtube/v3/videos - To update title
-   //https://www.googleapis.com/youtube/v3/videoCategories - To get category ID
   const token = req.user.access_token
   if (!token) {
     return res.status(401).json({ message: "Unauthorised access" })
   }
-  const { title } = req.body
-  if(!title){
-    return res.status(400).json({message: "title is required"})
+  const { title, description } = req.body
+  if(!title || !description){
+    return res.status(400).json({message: "title and description is required"})
   }
 
   try {
@@ -145,6 +143,7 @@ export const changeTitle = async(req,res)=>{
         id: "BuScJaRZxPg",
         snippet:{
           title: title,
+          description: description,
           categoryId: categoryId
         }
       }, 
@@ -165,3 +164,109 @@ export const changeTitle = async(req,res)=>{
     return res.status(status).json({ message })
   }
 }
+
+export const getComment=async(req,res)=>{
+  const token = req.user.access_token
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorised access" })
+  }
+  try {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/commentThreads',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+           part: 'snippet',
+           videoId: 'BuScJaRZxPg'
+         }
+      }
+    )
+    const comments = []
+    const itemLength = response?.data?.items?.length
+    for(let i=0; i<itemLength; i++){
+       
+        comments.push({
+           text: response?.data?.items[i]?.snippet?.topLevelComment?.snippet?.textDisplay,
+           id: response?.data?.items[i]?.id
+       })
+       }
+    
+      return res.status(200).json({ comments })
+  } catch (error) {
+    console.log(error)
+    const status = error?.response?.data?.error.code || 500
+    const message = error?.response?.data?.error.message || "Internal server error"
+    return res.status(status).json({ message })
+  }
+}
+
+export const deleteComment=async(req,res)=>{
+  const token = req.user.access_token
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorised access" })
+  }
+   const {id}=req.params
+   if(!id){
+    return res.status(400).json({message: "comment id is required"})
+   }
+   try {
+    const response = await axios.delete('https://www.googleapis.com/youtube/v3/comments',
+     {
+         headers: {
+           Authorization: `Bearer ${token}`
+         },
+         params: {
+            id: id
+          }
+       }
+    )
+    return res.status(200).json({message: "comment successfully deleted"})
+   } catch (error) {
+      console.log(error)
+      const status = error?.response?.data?.error.code || 500
+      const message = error?.response?.data?.error.message || "Internal server error"
+      return res.status(status).json({ message })
+   }
+}
+
+export const replyToComment=async(req,res)=>{
+  const {id}=req.params
+  const token = req.user.access_token
+  const { reply } = req.body
+  if(!reply){
+    return res.status(400).json({ message: "Comment reply is not attached" })
+  }
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorised access" })
+  }
+  if(!id){
+    return res.status(400).json({message: "Parent comment id is required"})
+  }
+  try {
+    const response = await axios.post('https://www.googleapis.com/youtube/v3/comments',
+      {
+        snippet:{
+           parentId: id,
+           textOriginal: reply
+        },
+        
+      },
+      {
+           headers: {
+             Authorization: `Bearer ${token}`
+           },
+           params: {
+              part: 'snippet'
+            }
+         }
+    )
+    return res.status(200).json({message: response.data})
+  } catch (error) {
+     console.log(error)
+      const status = error?.response?.data?.error.code || 500
+      const message = error?.response?.data?.error.message || "Internal server error"
+      return res.status(status).json({ message })
+  }
+
+} 
--- src/router/youtube.router.js
+++ src/router/youtube.router.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import { callback, changeTitle, getVideos, login, postComment } from "../controller/youtube.controller.js";
+import { callback, changeTitle, deleteComment, getComment, getVideos, login, postComment, replyToComment } from "../controller/youtube.controller.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
+
 
 
 const router=Router();
@@ -9,5 +10,8 @@ router.route('/login').get(login)
 router.route('/google/callback').get(callback)
 router.route('/getvideo').get(verifyToken,getVideos)
 router.route('/post-comment').post(verifyToken, postComment)
-router.route('/change-title').post(verifyToken,changeTitle)
+router.route('/change-title-desc').post(verifyToken,changeTitle)
+router.route('/getcomment').get(verifyToken,getComment)
+router.route('/delete-comment/:id').delete(verifyToken,deleteComment)
+router.route('/reply-to-comment/:id').post(verifyToken,replyToComment)
 export default router;
