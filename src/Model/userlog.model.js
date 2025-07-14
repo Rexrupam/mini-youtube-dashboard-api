9ec6fd8c94804e14ca0b1460d534820750f64857
rupam <rexrupam70@gmail.com>
2025-07-14 19:07:17 +0530

    some changes in code files

---
+++ src/Model/userlog.model.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const UserlogSchema = new mongoose.Schema({
+    customUrl:{
+        type: String,
+        required: true
+    },
+    channelId:{
+        type: String,
+        required: true
+    },
+    action:{
+        type: String
+    }
+}, {timestamps: true})
+
+export const User = mongoose.model('User', UserlogSchema)
